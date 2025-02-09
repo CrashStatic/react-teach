4256cca8b5679f752c4f433844595e48015187d5
CrashStatic <thecrashstatic@gmail.com>
2025-02-09 17:05:04 +0300

    Добавляет проверку на пустое значение имени и добавляет стиль ошибки

--- src/components/FeedbackSection.jsx
+++ src/components/FeedbackSection.jsx
@@ -4,9 +4,11 @@ import {useState} from "react";
 export default function FeedbackSection() {
   const [name, setName] = useState('');
   const [reason, setReason] = useState('help');
+  const [hasError, setHasError] = useState(false);
 
   function handleNameChange(event) {
     setName(event.target.value);
+    setHasError(event.target.value.trim().length === 0);
   }
 
   return (
@@ -14,7 +16,15 @@ export default function FeedbackSection() {
       <h2>Обратная связь</h2>
       <form>
         <label htmlFor='name'>Ваше имя</label>
-        <input type='text' id='name' className='control' value={name} onChange={handleNameChange} />
+        <input
+          type='text'
+          id='name'
+          className='control'
+          value={name}
+          style={{
+            border: hasError ? '1px solid red' : null,
+          }}
+          onChange={handleNameChange} />
 
         <label htmlFor='reason'>Причина обращения</label>
         <select id='reason' className='control' value={reason} onChange={event => setReason(event.target.value)}>
