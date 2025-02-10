2e2dacd7ea14a3973b624d23e587cd2b1fe6e033
CrashStatic <thecrashstatic@gmail.com>
2025-02-10 13:06:30 +0300

    Добавляет useEffect и удаление таймера в header для устранения утечки памяти

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,11 +1,15 @@
 import logo from '/logo-name.svg';
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import { HeaderContainer} from "./styles.js";
 
 export default function Header() {
   const [time, setTime] = useState(new Date());
 
-  setInterval(() => setTime(new Date()), 1000);
+  useEffect(() => {
+    const interval =  setInterval(() => setTime(new Date()), 1000);
+
+    return () => clearInterval(interval);
+  }, []);
 
   return (
     <HeaderContainer>
