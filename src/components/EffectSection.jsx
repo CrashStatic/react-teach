8d7765dc70149e5f99bb0ae40b2a9ff5f74c14df
CrashStatic <thecrashstatic@gmail.com>
2025-02-10 14:01:36 +0300

    Добавляет вывод пользователей через useEffect

--- src/components/EffectSection.jsx
+++ src/components/EffectSection.jsx
@@ -1,19 +1,29 @@
 import Button from "./button/Button.jsx";
 import Modal from "./modal/Modal.jsx";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 
 export default function EffectSection() {
   const [modal, setModal] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [users, setUsers] = useState([]);
 
-  function openModal() {
-    setModal(true);
-  }
+  useEffect(() => {
+    async function fetchUsers() {
+      setLoading(true);
+      const response = await fetch('https://jsonplaceholder.typicode.com/users');
+      const users = await response.json();
+      setUsers(users);
+      setLoading(false);
+    }
+
+    fetchUsers();
+  }, [])
 
   return (
     <section>
       <h2>Effects</h2>
 
-      <Button onClick={openModal}>Открыть информацию</Button>
+      <Button style={{marginBottom: '1rem'}} onClick={() => setModal(true)}>Открыть информацию</Button>
 
       <Modal open={modal}>
         <h2>Hello from modal</h2>
@@ -22,6 +32,15 @@ export default function EffectSection() {
         </p>
         <Button onClick={() => setModal(false)}>Close modal</Button>
       </Modal>
+
+      {loading && <p>Loading...</p>}
+
+      {!loading && (
+        <ul>
+          {users.map((user) => (
+            <li key={user.id}>{user.name}</li>
+          ))}
+        </ul>)}
     </section>
   )
 }
